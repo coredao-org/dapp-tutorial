44217c2b3ea3cbf5c65d7128a3e6ce704972be6b
David Chukwudi Onyeka (Nokia) <david.chukwudi_onyeka@nokia.com>
2025-05-29 10:01:29 +0100

    migrated-to-new-testnet

--- 02-Basic Staking Full Stack Dapp/hardhat.config.js
+++ 02-Basic Staking Full Stack Dapp/hardhat.config.js
@@ -16,9 +16,9 @@ module.exports = {
       hardhat: {
       },
       core_testnet: {
-         url: 'https://rpc.test.btcs.network',
+         url: 'https://rpc.test2.btcs.network',
          accounts: [PRIVATE_KEY],
-         chainId: 1115,
+         chainId: 1114,
       }
    },
    solidity: {
