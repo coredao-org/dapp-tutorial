302469a2cd88aa8cd1139d55a806bc404dbe1af2
gr-akshaya <akshayagangatharan@gmail.com>
2025-10-16 19:44:51 +0530

    removed tg mini dapp tutorial

--- Core TG Mini App SDK/package/README.md
+++
@@ -1,3 +0,0 @@
-# TON + CORE TELEGRAM MINI APP
-
-A boilerplate for making Telegram mini apps on core dao with react.js
--- Core TG Mini App SDK/template/src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div>
-      <h1>Telegram mini app on Core </h1>
-      <w3m-button />
-    </div>
-  );
-}
